496ad567e37595afd8616e749571964c3b16d051
Ziwei Wang <151121940+alicwang914@users.noreply.github.com>
2024-11-22 15:40:50 -0500

    updates

--- nba-fan-website/server.js
+++ nba-fan-website/server.js
@@ -9,7 +9,6 @@ app.use(express.static('public'));
 
 // Routes
 const playersRouter = require('./routes/players');
-const teamsRouter = require('./routes/teams');
 const gamesRouter = require('./routes/games');
 const shootersRouter = require('./routes/shooters');
 const playersPerformanceRouter = require('./routes/playersPerformance');
@@ -22,7 +21,6 @@ app.use('/api/game-results', gameResultsRouter);
 app.use('/api/shooters', shootersRouter);
 app.use('/api/players', playersRouter);
 app.use('/api/clutch-performance', clutchPerformanceRouter);
-app.use('/api/teams', teamsRouter);
 app.use('/api/games', gamesRouter);
 app.use('/api/playersPerformance', playersPerformanceRouter);
 
