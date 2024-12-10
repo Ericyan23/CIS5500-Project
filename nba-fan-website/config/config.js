03c81ad4086c3357d55e2295f3525d23c0ceaaa4
Navyfish67 <ash.jy.wang@gmail.com>
2024-12-10 16:59:05 -0500

    sdf

--- nba-fan-website/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rds_host: "project.cxbdfqmfqxtk.us-east-1.rds.amazonaws.com",
-    rds_port: 5432,
-    rds_user: "Project550",
-    rds_password: "Jhh887766.",
-    rds_db: "postgres",
-};
